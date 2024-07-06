56437c40f434ad75145cc62f98fbc1e8f4163b1f
lilian <lilliannzioki@gmail.com>
2024-07-06 10:54:15 +0300

    cookies-and-sessions-lab

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -12,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/clear')
@@ -22,13 +20,28 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    articles_list = [article.to_dict() for article in articles]
+    return jsonify(articles_list), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Set the initial value of 'page_views' to 0 if it doesn't exist
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment the value of 'page_views'
+    session['page_views'] += 1
+
+    # Check the number of page views
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    # Fetch the article from the database
+    article = Article.query.get(id)
+    if article:
+        return jsonify(article.to_dict()), 200
+    else:
+        return jsonify({'message': 'Article not found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555)
